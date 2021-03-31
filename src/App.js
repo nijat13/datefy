d7469eb5d88c07a9c031b869ebb90d54df9bfb8b
Nijat Ahmadov <na@ophigo.com>
2021-03-31 07:27:49 +0200

    Sass setup done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { getCompaniesApi } from './services/companies.service';
 import { useEffect } from 'react';
 
--- src/services/companies.service.js
+++ src/services/companies.service.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const getCompaniesApi = () => {
-  console.log('process.env: ', process.env.REACT_APP_API__SERVER);
   return axios({
     method: 'get',
     url: process.env.REACT_APP_API__SERVER + '/companies'
