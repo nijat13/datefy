ea67324cbc8e189dc78f17994eb8668f7d263395
Nijat Ahmadov <na@ophigo.com>
2021-03-31 08:06:57 +0200

    Header added to the app

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { getCompaniesApi } from '../../services/companies.service';
 import './index.scss';
+import Header from '../../components/Header';
 
 
 const fetchCompanies = async () => {
@@ -17,19 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/app';
+import App from './containers/home';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/scss/bootstrap.scss";
 
 ReactDOM.render(
   <React.StrictMode>
